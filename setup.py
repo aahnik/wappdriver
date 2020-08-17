b04866ae99cb9d869b9b3301b890328cdc2194fb
Aahnik Daw <aahnikdaw@gmail.com>
2020-08-17 22:20:30 +0530

    updated gitignore and setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aahnik/wapp-driver",
+    install_requires=['selenium'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
