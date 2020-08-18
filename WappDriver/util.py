2ef69dc3d078baf84c2929dfbcf468c22094582b
Aahnik Daw <aahnikdaw@gmail.com>
2020-08-18 07:33:50 +0530

    added a method for first time setup

--- WappDriver/util.py
+++ WappDriver/util.py
@@ -1,3 +1,15 @@
+def first_time_set_up(up):
+    print("\n Thank You for using wapp-driver. Welcome !! ")
+    print(
+        "\n You have to enter the Chrome Driver Path once: only for the first time")
+    up.update_cdp()
+    print("Sucessfully Saved")
+    up.fetch_vars()
+    print("Latest values of Dynamic Variables fetched from internet !\n")
+    print("If you want to change Chrome Driver Path or update Dynamic Variables")
+    print("     then please visit    aahnik.github.io/wapp-driver ")
+
+
 def convey(error, message):
     print(f"\n {message} \n")
     print("For Help Contact Aahnik Daw, the author...")
@@ -6,5 +18,3 @@ def convey(error, message):
 
     if input("Enter d to see details about Exceptions caught.\n") == 'd':
         print(f'\n{error}\n')
-
-
