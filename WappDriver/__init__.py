a9b5b4ef389b490138f44b51f0e70d744c088c4f
Aahnik Daw <aahnikdaw@gmail.com>
2020-08-18 06:59:07 +0530

    added data files, added update.py,  filled up init

--- WappDriver/__init__.py
+++ WappDriver/__init__.py
@@ -1,2 +1,15 @@
 from .driver import WappDriver
+from .update import local_varVer_val, update_cdp, fetch_vars
+
+if local_varVer_val == 0.0:
+    print("\n Thank You for using wapp-driver. Welcome !! ")
+    print("\n You have to enter the Chrome Driver Path once: only for the first time")
+    update_cdp()
+    print("Sucessfully Saved")
+    fetch_vars()
+    print("Latest values of Dynamic Variables fetched from internet !\n")
+    print("If you want to change Chrome Driver Path or update Dynamic Variables")
+    print("     then please visit    aahnik.github.io/wapp-driver ")
+
+
 # AAHNIK 2020
---
+++ WappDriver/update.py
@@ -0,0 +1,37 @@
+# this module provides functions to  make the data files dynamic
+
+import pkg_resources
+import requests
+
+token = 'd9fb98d88f620d0320bf305ada414299/raw/616fadf34d3a46ae6e02b63580e0d318d0a4a0cf'
+
+URL = f'https://gist.githubusercontent.com/aahnik/{token}/'
+
+remote_varVer = f'{URL}wapp-driver-var-ver'
+remote_var = f'{URL}wapp-driver-var.yml'
+
+P = 'wapp-driver'
+
+var = pkg_resources.resource_filename(P, 'data/var.yml')
+chrome_driver_path = pkg_resources.resource_filename(
+    P, 'data/chrome_driver_path.txt')
+varVer = pkg_resources.resource_filename(P, 'data/varVer')
+
+local_varVer_val = float(open(varVer).readline())
+
+
+def update_cdp():
+    # cdp stands for chrome_driver_path
+    path = input("Paste the absoulte path of Chrome Driver: \n")
+    with open(chrome_driver_path, 'w') as f:
+        f.write(path)
+
+
+def fetch_vars():
+    latest_varVer = float(requests.get(url=remote_varVer).text)
+    if latest_varVer > local_varVer_val:
+        open(var, 'w').write(requests.get(url=remote_var).text)
+        open(varVer, 'w').write(str(latest_varVer))
+        return("updated sucessfully")
+    else:
+        return("already the latest version")
