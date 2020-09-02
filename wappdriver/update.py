790b86b9be07d341e4298437f8d90462d13bf806
Aahnik Daw <aahnikdaw@gmail.com>
2020-09-02 17:15:15 +0530

    a

--- wappdriver/update.py
+++ wappdriver/update.py
@@ -3,9 +3,9 @@
 import pkg_resources
 import requests
 
-token = 'd9fb98d88f620d0320bf305ada414299/raw/616fadf34d3a46ae6e02b63580e0d318d0a4a0cf'
+prefix = 'd9fb98d88f620d0320bf305ada414299/raw/616fadf34d3a46ae6e02b63580e0d318d0a4a0cf'
 
-URL = f'https://gist.githubusercontent.com/aahnik/{token}/'
+URL = f'https://gist.githubusercontent.com/aahnik/{prefix}/'
 
 remote_varVer = f'{URL}wapp-driver-var-ver'
 remote_var = f'{URL}wapp-driver-var.yml'
