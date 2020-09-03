e8b32e43cd2a5d3867ecace05f88722b43cccb41
Aahnik Daw <aahnikdaw@gmail.com>
2020-09-03 19:54:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="wappdriver",
-    version="0.2.3",
+    version="0.2.4",
     license='MIT',
     author="Aahnik Daw",
     author_email="aahnikdaw@gmail.com",
