fafd758addce6a10915311683b2f849eef780d9b
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-09 20:32:59 +0530

    add beautiful display via pyfiglet

--- wappdriver/util.py
+++ wappdriver/util.py
@@ -1,17 +1,19 @@
-def first_time_set_up(up):
-    print("\n Thank You for using wapp-driver. Welcome !! ")
+from . import update
+from pyfiglet import Figlet
+
+def first_time_set_up():
+    f = Figlet(font='big')
+    print(f.renderText('wapp'))
+    print(f.renderText('driver'))
+
     print(
         "\n You have to enter the Chrome Driver Path once: only for the first time")
-    up.update_cdp()
+    update.update_cdp()
     print("Sucessfully Saved")
-    up.fetch_vars()
-    print("Latest values of Dynamic Variables fetched from internet !\n")
-    print("If you want to change Chrome Driver Path or update Dynamic Variables")
-    print("     then please visit    aahnik.github.io/wappdriver ")
-
+    update.fetch_vars()
+    
 
 def convey(error, message):
     print(f"\n {message} \n")
     print(f'\n{error}\n')
     print("\n For help visit aahnik.github.io/wappdriver ")
-
