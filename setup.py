348d19ee0f69609e978118a5537156debc3f4fa5
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-09 20:46:51 +0530

    start working to version 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="wappdriver",
-    version="0.2.7",
+    version="0.3.0",
     license='MIT',
     author="Aahnik Daw",
     author_email="meet.aahnik@gmail.com",
