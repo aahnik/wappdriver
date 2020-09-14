aa34a1a54f43960c970501fac5c76c3089d83f8c
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-14 10:08:02 +0530

    create custom exception class and logging feature

---
+++ wappdriver/error.py
@@ -0,0 +1,62 @@
+''' 
+This module error.py defines custom Wappdriver Exception. 
+WappDriver Excpetion helps in abstracting internal exception details from the end user
+'''
+
+# from wappdriver.data import local
+import logging
+
+
+
+logging.basicConfig(format='\n\n%(asctime)s - %(message)s',
+                    filename=local.log_file)
+
+
+class WappDriverError(Exception):
+    '''
+    Exception raised for errors in functioning of WappDriver.
+
+    Attributes:
+    internal - - actual exceptions which are being abstracted away from end user
+    err  - -  what went wrong
+    message -- custom message explainng what could have caused the error and how to resolve it
+
+    ----------
+
+    Use : When any error is caught in WappDriver, an WappDriverError is raised.
+
+    ```python
+
+    try:
+        d = 2/0
+        # code that might trigger some error
+
+    except Exception as internal:   
+        try:
+            raise WappDriverError(internal=internal,
+                                problem="could not load this / this failed ...",
+                                message="check that ... / have you ..? ")
+        except Exception as err:
+            print(err)
+    ```
+
+    Raising WappDriverError automatically logs the entire internal error messages with timestamps.
+    The logs are not displayed to the user, rather they are appended to the log file.
+
+
+
+    '''
+
+    def __init__(self, internal, problem, message):
+        self.internal = internal
+        self.problem = problem
+        self.message = message
+        super().__init__(self.message)
+
+        logging.exception(f'{problem}\n{internal}')
+
+    def __str__(self):
+        return f'\n{self.message}\nFor Help Visit https://aahnik.github.io/wappdriver/docs/help.html\n'
+
+
+
