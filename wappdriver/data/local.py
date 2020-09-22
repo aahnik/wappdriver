f6922bec6c21af8d6a650861db8c100ed1e49bff
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-23 02:40:46 +0530

    local module
    for accessing data on system
    
    on importing local
    existence of required directories is ensured

--- wappdriver/data/local.py
+++ wappdriver/data/local.py
@@ -1,8 +1,22 @@
 '''This module local.py provides access to local data. It can read and write local data'''
+import os
+from datetime import datetime
+from .. import __version__
 
-# log_file = \path\
-    
-def local_ensure():
-    pass
 
-    
+dir = os.path.expanduser('~/.wappdriver')
+log_file = os.path.join(dir, 'log_file.txt')
+
+
+if not os.path.exists(dir):
+    os.mkdir(dir)
+
+if not os.path.exists(log_file):
+    with open(log_file, 'w+') as f:
+        f.write(f'''
+        --------------------------
+        Log File Created
+        {datetime.now()}
+        wappdriver : {__version__}
+        ---------------------------\n
+        ''')
