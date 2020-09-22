61bd967e9a4f2c5177e03c2163043d0db1fd25ee
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-23 02:37:36 +0530

    error module styled

--- wappdriver/error.py
+++ wappdriver/error.py
@@ -3,12 +3,11 @@ This module error.py defines custom Wappdriver Exception.
 WappDriver Excpetion helps in abstracting internal exception details from the end user
 '''
 
-# from wappdriver.data import local
+from .data import local
 import logging
 
 
-
-logging.basicConfig(format='\n\n%(asctime)s - %(message)s',
+logging.basicConfig(format='\n########################################\n\n%(asctime)s - %(message)s',
                     filename=local.log_file)
 
 
@@ -53,10 +52,15 @@ class WappDriverError(Exception):
         self.message = message
         super().__init__(self.message)
 
-        logging.exception(f'{problem}\n{internal}')
+        logging.exception(f'''\n{problem}\n\n{internal}''')
 
     def __str__(self):
-        return f'\n{self.message}\nFor Help Visit https://aahnik.github.io/wappdriver/docs/help.html\n'
+        return f'''
+        ------------------------------------------------------------------------------------
+        WappDriver Error Occured
 
+        {self.message}
 
-
+        For Help Visit https://aahnik.github.io/wappdriver/docs/help.html
+        -----------------------------------------------------------------------------------\n
+        '''
