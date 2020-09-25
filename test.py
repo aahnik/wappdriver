f6bcc6d6aa4ec2e3eb3c28648d6ced34f07136dd
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-25 23:27:46 +0530

    add test

---
+++ test.py
@@ -0,0 +1,17 @@
+# make sure to change the values before running
+
+from wappdriver import WhatsApp
+
+with WhatsApp() as bot:
+    bot.send('aahnik',  # name of recipient
+             'hi send by a bot',  # message
+
+             # absolute url of an image that exists on your computer
+             '/home/aahnik/Pictures/image_icon.png',  # 115 Kb
+
+             # absolute url of a video that exists on your computer
+             '/home/aahnik/Downloads/example_types/file_example_MP4_640_3MG.mp4',  # 3.1 MB
+
+             # absolute url of a pdf that exists on your computer
+             '/home/aahnik/Downloads/docs/django.pdf'  # 7 MB
+             )
