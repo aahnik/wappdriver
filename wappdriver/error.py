661e7984f30e3bd94b054ec7c55e8a3a2639cbd7
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-09-25 21:40:25 +0530

    new features added send image, video, files etc

--- wappdriver/error.py
+++ wappdriver/error.py
@@ -8,7 +8,7 @@ prevent cyclic import
 
 When you import this module or any function from this module, local is ensured 😊
 '''
-
+import functools
 from .data import local
 import logging
 
@@ -26,18 +26,17 @@ class WappDriverError(Exception):
     ### Attributes
 
     internal - - actual exceptions and full traceback which are being abstracted away from end user
-    
+
     problem  - -  what could have went wrong according to the developer 
-    
-    message -- custom message explainng what could have caused the error and how to resolve it
+
+    message -- custom message explainng what could have caused the error and how to resolve it. When displayed, message is prefixed with the words `Make sure to have:` 
 
     ---
     - The `problem` and current time will be the heading of any particular log
     - `internal` is not shown to the user, but logged.
 
     ---
-    Raising an WappDriverError automatically logs the entire internal error messages with timestamps.
-    The logs are not displayed to the user, rather they are appended to the log file.
+    Raising an WappDriverError automatically logs the entire internal error messages with timestamps.The logs are not displayed to the user, rather they are appended to the log file.
     '''
 
     def __init__(self, internal, problem, message):
@@ -50,12 +49,10 @@ class WappDriverError(Exception):
 
     def __str__(self):
         return f'''
+        Please check Internet Connection and always use the latest version of wappdriver.
         ------------------------------------------------------------------------------------
-        WappDriver Error : {self.problem}
-
-        {self.message}
-
-        Please make sure to use the latest version of wappdriver
+        WappDriver Error  : {self.problem} \n
+        Make sure to have : {self.message} \n
         For Help Visit https://aahnik.github.io/wappdriver/docs/help.html
         -----------------------------------------------------------------------------------\n
         '''
@@ -64,14 +61,13 @@ class WappDriverError(Exception):
 def handle_errors(problem, message):
     '''
     ### How to use ?
-    Write the vulnerable code inside a function. And decorate it with `handle_error`
-    There should be no `return` statement inside the vulnerable function.
-    The intended use case is for a procedural function.
+    Write the vulnerable code inside a function. And decorate it with `handle_error`. There should be no `return` statement inside the vulnerable function. The intended use case is for a procedural function.
 
     ---
     Example use 
     ```python
-    @handle_errors(problem='Could not...',message='Please ...')
+    # Note: when displayed, message is prefixed with the words `Make sure to have:` 
+    @handle_errors(problem='this not done',message='that')
     def vulnerable_function(arg):
         print('fishy errors')
         print(1/0)
@@ -87,16 +83,20 @@ def handle_errors(problem, message):
     ---
     `handle_errors` is a Decorator Factory which is used to create a decorator that takes arguments
     '''
-    
+
     def decorator_func(vulnerable_func):
         '''
         Actual Decorator to handle errors. It is nested inside a decorator factory, 
         as this decorator needs to take two arguments.
 
         '''
+        @functools.wraps(vulnerable_func)
         def wrapper_func(*args, **kwargs):
             '''
-            Wrapper Block that wraps any vulnerable code in WappDriver.
+            Wrapper Function to wrap vulnerable functions in WappDriver.
+            This is internally used by WappDriver. 
+            
+            Not intended to be used by users of WappDriver
             '''
             try:
                 vulnerable_func(*args, **kwargs)
@@ -108,6 +108,5 @@ def handle_errors(problem, message):
                     # Beautified Error message is printed and False is returned to caller
                     print(err)
                     return False
-
         return wrapper_func
     return decorator_func
