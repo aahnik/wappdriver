585f828082f3dba0d905a0ae225053c0412939c7
Aahnik Daw <66209958+aahnik@users.noreply.github.com>
2020-10-20 11:17:36 +0530

    auto-update Tue 20 Oct 11:17:36 IST 2020

--- wappdriver/error.py
+++ wappdriver/error.py
@@ -42,8 +42,6 @@ class WappDriverError(Exception):
         self.message = message
         super().__init__(self.message)
 
-        local.ensure()
-
         logging.basicConfig(format='\n########################################\n\n%(asctime)s - %(message)s',
                             filename=local.log_file)
 
