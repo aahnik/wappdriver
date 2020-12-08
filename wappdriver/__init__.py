35cdb3563d928e96d247025c4957a13417ec1944
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-08 10:00:54 +0530

    bump version

--- wappdriver/__init__.py
+++ wappdriver/__init__.py
@@ -18,7 +18,7 @@ If you wish to just use wappdriver, read the [Usage Guide](https://aahnik.github
 
 '''
 
-__version__ = "0.6.4"
+__version__ = "0.6.5"
 
 # from .context import Wapp
 from .local import set_chrome_driver_path, update_selectors
